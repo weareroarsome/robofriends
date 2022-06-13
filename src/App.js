195670c605d56665a2d372472f418ace96b83146
weareroarsome <butler_mail@icloud.com>
2022-06-13 13:15:15 +0100

    configure react app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, {Component} from "react";
 import CardList from './CardList';
-import { robots } from "./robots";
-import SearchBox from './SearchBox'
+// import { robots } from "./robots";
+import SearchBox from './SearchBox';
+import './App.css';
+import Scroll from './Scroll';
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {this.setState({robots : users})});
     }
 
     onSearchChange = (event) => {
@@ -17,16 +25,22 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots =>{
+        const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
         })
-        return (
-            <div className='tc'>
-                <h1> RoboFriends </h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} /> 
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return <h1> Loading </h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'> RoboFriends </h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll> 
+                        <CardList robots={filteredRobots} /> 
+                    </Scroll>
+                </div>
+                );
+        }
     }
 }
 
